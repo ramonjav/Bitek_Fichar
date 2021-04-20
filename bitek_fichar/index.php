87ef1b6fd2f1d4e82b30d15d596f6013d8305fb4
ramonjav <50997941+ramonjav@users.noreply.github.com>
2021-04-20 20:47:56 +0200

    Add files via upload

---
+++ bitek_fichar/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+
+    <title>Bitek</title>
+</head>
+<body>
+
+<form action="php/login.php" method="post">
+
+    <p>Correo: <input type="text" name="correo" /></p>
+    <p>Contraseña: <input type="password" name="contrasena" /></p>
+    <p><input type="submit" value="Acceder"/></p>
+
+</form>
+
+<?php
+if(isset($_GET['err'])){
+    echo "<span style='background-color:#ffffff; padding:10px; margin-top:30px; border-radius:10px; color:black; border:solid 3px #960a0a;'><strong> Datos incorrectos</strong></span>";
+}
+?>
+    <script src="js/jquery-3.6.0.slim.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
