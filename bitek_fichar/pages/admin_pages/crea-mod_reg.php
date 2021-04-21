c9af696268bc139f6b45df138c62984d21203eae
ramonjav <ramondav.24@gmail.com>
2021-04-21 11:30:36 +0200

    commit

--- bitek_fichar/pages/admin_pages/crea-mod_reg.php
+++ bitek_fichar/pages/admin_pages/crea-mod_reg.php
@@ -23,20 +23,61 @@
 
         if(isset($_GET['mod'])){
             $reg_id = $_GET['mod'];
-            $sql=mysqli_query($conexion,"SELECT * FROM usuarios WHERE id_user = $reg_id");
+            $sql=mysqli_query($conexion,"SELECT * FROM registro WHERE id_reg = '$reg_id'");
 
 	        if($row=mysqli_fetch_array($sql)){
                 
                 $FECHA=$row["fecha"];
                 $HORA = $row["hora"];
                 $ACCION=$row["accion"];
-                $ACEMP=$row["nombre"];
-                $ACEM=$row["apellidos"];
+                $ACEMP=$row["aceptado_trabajador"];
+                $ACEM=$row["aceptado_empresa"];
                 $ESTADO=$row["estado"];
             }
         }else{
-            echo "creando";
+            $FECHA='';
+            $HORA='';
+            $ACCION='';
+            $ACEMP='';
+            $ACEM='';
+            $ESTADO='';
         }
+
+        echo"
+        <form action='crea-mod_reg.php' method='post'>
+            <!--FECHA -->
+            <p><label for='name'>Fecha: </label>
+            <input type='name' name='fecha' value='$FECHA'></p>
+
+            <!-- HORA -->
+            <p><label for='name'>Hora: </label>
+            <input type='name' name='hora' value='$HORA'></p>
+
+            <!-- ACCION -->
+            <p><label for='name'>Acción: </label>
+            <input type='name' name='accion' value='$ACCION'></p>
+
+            <!-- ACEP_EMPLEADO -->
+            <p><label for='name'>Aceptado empleado: </label>
+            <input type='name' name='acemp' value='$ACEMP'></P>
+
+            <!-- ACEP_EMPRESA -->
+            <p><label for='name'>Aceptado Empresa: </label>
+            <input type='name' name='acem' value='$ACEM'></p>
+
+            <!-- Tipo -->
+            <p><label for='name'>Estado:</label>
+            <input type='name' name='estado' value='$ESTADO'></p>
+
+            <!-- Tipo -->
+            <p><label for='name'>Estado:</label>
+            <input type='name' name='estado' value='$ESTADO'></p>
+        
+            <input type='submit' name= 'guardar' value='Guardar'>
+            <input type='submit' name= 'eliminar' value='Eliminar'>
+            <input type='submit' name= 'nuevo' value='Nuevo Registro'>
+        
+        </form>";
 
     ?>
 
