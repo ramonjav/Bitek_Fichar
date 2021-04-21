01ec473002cff5872897be95ec0290465db06051
Walteryoel921 <Walteryoel921@gmail.com>
2021-04-21 17:21:37 +0200

    commit

--- bitek_fichar/index.php
+++ bitek_fichar/index.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css" />
-
     <title>Bitek</title>
 </head>
+<img src="/img/logo1.png" width="300px" height="100px" id="logo">
 <body>
 
 <form action="php/login.php" method="post">
