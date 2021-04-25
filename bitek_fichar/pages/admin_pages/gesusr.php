b73eb2a182edcf294767f3766c81d3d59d7446b7
Walteryoel921 <Walteryoel921@gmail.com>
2021-04-25 20:17:07 +0200

    buena

--- bitek_fichar/pages/admin_pages/gesusr.php
+++ bitek_fichar/pages/admin_pages/gesusr.php
@@ -17,8 +17,8 @@
             <li><div class="icon_1 with_img_200 buttonbg" style="width: 230px;"><a class="button_1" href="../gestor.php"></a></div></li>
             <li><div class="buttonbg" style="width: 120px;"><a href="gesusr.php">Gestión Usuario</a></div></li>
             <li><div class="buttonbg" style="width: 175px;"><a href="../notificaciones.php">Gestión de Notificaciones</a></div></li>
-            <li><div class="buttonbg" style="width: 87px;"><a href="admin_pages/consultas.php">Consultas</a></div></li>
-            <li><div class="buttonbg" style="width: 147px;"><a href="admin_pages/gesreg.php">Gestión de Registros</a></div></li>
+            <li><div class="buttonbg" style="width: 87px;"><a href="consultas.php">Consultas</a></div></li>
+            <li><div class="buttonbg" style="width: 147px;"><a href="gesreg.php">Gestión de Registros</a></div></li>
             <li><div class="buttonbg"><a href="../../php/session_destroy.php">Cerrar Sesión</a></div></li>
         </ul>
     </div>
