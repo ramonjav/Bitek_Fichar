4e5b5dcd6fa98d850328eb3bd8dcc2385349a792
ramonjav <ramondav.24@gmail.com>
2021-05-12 17:30:56 +0200

    vbmcghx

--- bitek_fichar/api/api_login.php
+++ bitek_fichar/api/api_login.php
@@ -1,10 +1,10 @@
 <?php 
 include("../php/conexion.php");
 
-if(!empty($_GET["correo"]) && !empty($_GET["contrasena"])){
+//if(!empty($_POST["correo"]) && !empty($_POST["contrasena"])){
 
-    $email=strip_tags(mysqli_real_escape_string($conexion,$_GET['correo']));
-	$pass=strip_tags(mysqli_real_escape_string($conexion,$_GET['contrasena']));
+    $email=$_POST['correo'];
+	$pass=$_POST['contrasena'];
     
 	$sql=mysqli_query($conexion,"SELECT * FROM usuarios WHERE correo='$email' and contrasena='$pass'");
 
@@ -21,26 +21,26 @@ if(!empty($_GET["correo"]) && !empty($_GET["contrasena"])){
             $response["name"] = $row["nombre"];
             $response["lname"] = $row["apellidos"];
 
-            die(json_encode($response));
+            echo json_encode($response);
         }else{
             $response["success"] = 0;
             $response["message"] = "El usuario que seleccionó no esta activo. Contacta con tu administrador para activar este usuario";
-            die(json_encode($response));
+            //echo json_encode($response);
     
         }
  
 	}else{
         $response["success"] = 0;
         $response["message"] = "Credenciales incorrectas";
-        die(json_encode($response));
+        //echo json_encode($response);
 
 	}
-}else{
+/*}else{
     $response["success"] = 0;
     $response["message"] = "No puedes dejar campos vaciós";
-    die(json_encode($response));
+    //secho json_encode($response);
 
-}
+}*/
 
 
 ?>
